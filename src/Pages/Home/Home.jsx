24032714ae9d0cc84a0059b44de01b6e8bcbe4f7
nhwnahid007 <nhwnahid@gmail.com>
2024-04-29 08:15:06 +0600

    Added tourist card in the home section

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -3,14 +3,15 @@ import TouristCard from "../../components/TouristCard/TouristCard";
 
 const Home = () => {
   const tourists = useLoaderData();
+  const limitedTourists = tourists.slice(0, 6);
   return (
     <div>
       <h1>This is home </h1>
       <p>{tourists.length}</p>
 
-    <div className="grid lg:grid-cols-2">
+    <div className="grid bg-base-200 lg:grid-cols-2">
         {
-            tourists.map(tourist => <TouristCard key={tourist.id} tourist={tourist} ></TouristCard>)
+            limitedTourists.map(tourist => <TouristCard key={tourist.id} tourist={tourist} ></TouristCard>)
         }
     </div>
       
--- src/components/TouristCard/TouristCard.jsx
+++ src/components/TouristCard/TouristCard.jsx
@@ -9,16 +9,16 @@ const TouristCard = ({ tourist }) => {
           <a href="#">
             <img className="w-[600px] h-[300px] lg:h-[400px]" src={photo} alt="spot photo" />
           </a>
-          <div className="relative -mt-16 px-10 pt-2 pb-16 bg-white m-10">
+          <div className="relative rounded-lg -mt-16 px-10 pt-2 pb-16 bg-white m-10">
             <a
               href="#"
               className="font-semibold font-lora text-lg inline-block hover:text-indigo-600 transition duration-500 ease-in-out"
             >
               {spot}
             </a>
-            <p className="text-gray-500 text-sm">{location},{country}</p>
+            <p className="text-gray-500 text-sm">{location}, {country}</p>
             <p className="text-gray-500 text-sm">For {time} price of {price} $ Only</p>
-            <button className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
+            <button className="relative inline-flex items-center justify-center p-0.5 mt-2 me-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-pink-200 dark:focus:ring-pink-800">
               <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
                 View Details
               </span>
